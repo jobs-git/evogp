3c029cf3b22e823ae1c11f4624106cc1dca62382
wls2002 <cvgcvg@163.com>
2025-01-07 16:40:58 +0800

    modify base interface

--- src/evogp/algorithm/crossover/base.py
+++ src/evogp/algorithm/crossover/base.py
@@ -1,5 +1,13 @@
+import torch
 from ...tree import Forest
 
+
 class BaseCrossover:
-    def __call__(self, forest: Forest):
+    def __call__(
+        self,
+        forest: Forest,
+        survivor_indices: torch.Tensor,
+        target_cnt: int,
+        fitness: torch.Tensor,
+    ) -> Forest:
         raise NotImplementedError
--- src/evogp/algorithm/crossover/default.py
+++ src/evogp/algorithm/crossover/default.py
@@ -9,22 +9,40 @@ class DefaultCrossover(BaseCrossover):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, forest: Forest, target_cnt: int, args_check: bool = True):
-        forest_size = forest.pop_size
-
+    def __call__(
+        self,
+        forest: Forest,
+        survivor_indices: torch.Tensor,
+        target_cnt: int,
+        fitness: torch.Tensor,
+        args_check: bool = True,
+    ):
+        survivor_forest = forest[survivor_indices]
         # choose left and right indices
         left_indices, right_indices = torch.randint(
-            low=0, high=forest_size, size=(2, target_cnt), dtype=torch.int32, device="cuda", requires_grad=False
+            low=0,
+            high=len(survivor_forest),
+            size=(2, target_cnt),
+            dtype=torch.int32,
+            device="cuda",
+            requires_grad=False,
         )
 
         # choose left and right positions
-        tree_sizes = forest.batch_subtree_size[:, 0]
+        tree_sizes = survivor_forest.batch_subtree_size[:, 0]
         left_pos_unlimited, right_pos_unlimited = torch.randint(
-            low=0, high=MAX_STACK, size=(2, target_cnt), dtype=torch.int32, device="cuda", requires_grad=False
+            low=0,
+            high=MAX_STACK,
+            size=(2, target_cnt),
+            dtype=torch.int32,
+            device="cuda",
+            requires_grad=False,
         )
         left_pos = left_pos_unlimited % tree_sizes[left_indices]
         right_pos = right_pos_unlimited % tree_sizes[right_indices]
 
         # print(f"{tree_sizes=}\n{left_indices=}\n{right_indices=}\n{left_pos=}\n{right_pos=}")
 
-        return forest.crossover(left_indices, right_indices, left_pos, right_pos, args_check=args_check)
+        return survivor_forest.crossover(
+            left_indices, right_indices, left_pos, right_pos, args_check=args_check
+        )
