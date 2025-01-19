144c9645532f466cb413816db3b163c3899baf6a
wls2002 <cvgcvg@163.com>
2025-01-19 16:34:25 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 
-os.environ["MAX_JOBS"] = "16"
+cpu_count = os.cpu_count()
+if cpu_count:
+    os.environ["MAX_JOBS"] = str(cpu_count)
+else:
+    os.environ["MAX_JOBS"] = "1"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="evogp",
+    version="0.1.0",
+    author="Lishuang Wang, Zhihong Wu, Kebin Sun",
+    author_email="zhihong2718@gmai.com",
+    description="Evolutionary Genetic Programming with CUDA-based GPU acceleration.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EMI-Group/evogp",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     ext_modules=[
         CUDAExtension(
             name="evogp_cuda",
@@ -31,5 +47,11 @@ setup(
         )
     ],
     cmdclass={"build_ext": BuildExtension.with_options(parallel=True)},
-    options={"build_ext": {"build_lib": "./src/evogp"}},
+    # options={"build_ext": {"build_lib": "./src/evogp"}},
+    install_requires=[
+        "torch",
+        "numpy",
+    ],
+    include_package_data=True,
+    python_requires=">=3.12",
 )
