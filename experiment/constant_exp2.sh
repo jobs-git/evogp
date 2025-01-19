85538dbd52b2d7a5e49761f13e0d767037d994fe
wls2002 <cvgcvg@163.com>
2025-01-19 15:42:11 +0800

    refactor project structure

--- experiment/constant_exp2.sh
+++
@@ -1,4 +0,0 @@
-cd /wanglishuang/working_space/evogp/;
-export PYTHONPATH="./src";
-/root/miniconda3/envs/evogp_env/bin/python setup.py build_ext --inplace;
-/root/miniconda3/envs/evogp_env/bin/python ./experiment/constant_exp2.py;
